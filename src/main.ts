0a17d312a554745382b9b205a32a8685d5b8cc4d
Lucaswillians <lucaswilianserpa@gmail.com>
2025-11-13 18:24:21 -0300

    chore: loggers config

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { CloudLogger } from './logger/cloud.logger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: new CloudLogger('Backend'),
+  });
+
   app.enableCors({
-    origin: ["https://vanguardsystem.com.br", "https://www.vanguardsystem.com.br", "http://localhost:5173"],
+    origin: [
+      "https://vanguardsystem.com.br",
+      "https://www.vanguardsystem.com.br",
+      "http://localhost:5173",
+    ],
     credentials: true,
   });
+
   app.use(cookieParser());
   await app.listen(3000);
 }
