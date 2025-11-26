208e2df2971d11c8501ff862c920b887fefde795
Lucaswillians <lucaswilianserpa@gmail.com>
2025-11-25 22:24:34 -0300

    fix: adjusts at tests gasapi and geocode

--- src/geocodeApi/test/geocodeApi.service.spec.ts
+++ src/geocodeApi/test/geocodeApi.service.spec.ts
@@ -1,32 +1,68 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { HttpModule } from '@nestjs/axios';
 import { GeocodeApiService } from './../geocodeApi.service';
+import { HttpService } from '@nestjs/axios';
+import { AxiosResponse, AxiosHeaders } from 'axios';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
 
 describe('GeocodeApiService', () => {
   let service: GeocodeApiService;
+  let httpService: any;
+  let cacheMock: any;
 
-  beforeAll(async () => {
+  beforeEach(async () => {
+    httpService = {
+      axiosRef: {
+        get: jest.fn(),    // <- mock REAL
+      }
+    };
+
+    cacheMock = {
+      get: jest.fn().mockResolvedValue(null),
+      set: jest.fn().mockResolvedValue(undefined),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      imports: [HttpModule],
-      providers: [GeocodeApiService],
+      providers: [
+        GeocodeApiService,
+        { provide: HttpService, useValue: httpService },
+        { provide: CACHE_MANAGER, useValue: cacheMock },
+      ],
     }).compile();
 
     service = module.get<GeocodeApiService>(GeocodeApiService);
   });
 
   it('deve retornar as coordenadas de Florianópolis', async () => {
-    const city = 'Joinville SC';
-    const result = await service.getCoordinates(city);
+    const mockResponse: AxiosResponse = {
+      data: [{ lat: "-27.5945", lon: "-48.5477" }],
+      status: 200,
+      statusText: 'OK',
+      headers: new AxiosHeaders(),
+      config: { headers: new AxiosHeaders() }
+    };
 
-    expect(result).toHaveProperty('lat');
-    expect(result).toHaveProperty('lng');
-    expect(typeof result.lat).toBe('number');
-    expect(typeof result.lng).toBe('number');
+    httpService.axiosRef.get.mockResolvedValueOnce(mockResponse);
+
+    const result = await service.getCoordinates('Florianópolis');
+
+    expect(result).toEqual({
+      lat: -27.5945,
+      lng: -48.5477,
+    });
   });
 
   it('deve lançar erro se a cidade não existir', async () => {
-    await expect(service.getCoordinates('CidadeInexistente123')).rejects.toThrow(
-      /não encontrada/,
-    );
+    const mockResponse: AxiosResponse = {
+      data: [],
+      status: 200,
+      statusText: 'OK',
+      headers: new AxiosHeaders(),
+      config: { headers: new AxiosHeaders() }
+    };
+
+    httpService.axiosRef.get.mockResolvedValueOnce(mockResponse);
+
+    await expect(service.getCoordinates('CidadeInexistente123'))
+      .rejects.toThrow('Cidade "CidadeInexistente123" não encontrada');
   });
 });
